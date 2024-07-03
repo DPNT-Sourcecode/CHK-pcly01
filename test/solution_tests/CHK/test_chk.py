c0ec239fd4232ad035731f1d9fef8533207447ae
sbx_user1051 <sbx_user1051@169.254.55.165>
2024-07-03 20:30:15 +0000

    Wed Jul 03 20:30:15 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,5 +1,16 @@
 from lib.solutions.CHK import checkout_solution
+import pytest
 
 class TestChk():
     def test_chk(self):
-        assert checkout_solution.checkout('A A B C D') == 
+        assert checkout_solution.checkout('A B C D') == 115
+
+    def test_chk_three_a_offer(self):
+        assert checkout_solution.checkout('A A A') == 130
+
+    def test_chk_two_b_offer(self):
+        assert checkout_solution.checkout('B B') == 45
+
+    def test_chk_invalid_letter(self):
+            with pytest.raises(ValueError):
+                checkout_solution.checkout('E')
