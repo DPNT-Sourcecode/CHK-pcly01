6743bf87dd2f6237ceaa1cd6d34c458fcddc190f
sbx_user1051 <sbx_user1051@169.254.119.197>
2024-07-03 15:44:08 +0000

    Wed Jul 03 15:44:08 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -9,7 +9,14 @@ class TestSum():
     def test_sum_upperbounds(self):
         assert sum_solution.compute(100, 100) == 200
     
+    def test_sum_lowerbounds(self):
+        assert sum_solution.compute(0, 0) == 0
+    
     def test_sum_above_limit(self):
-        self.assertRaises()
+        self.assertRaises(ValueError, sum_solution.compute(101, 100))
+        
+    def test_sum_negative_integer(self):
+        self.assertRaises(ValueError, sum_solution.compute(-1, 10))
+
 
 
