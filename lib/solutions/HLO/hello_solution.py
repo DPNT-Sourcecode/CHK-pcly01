889ee1f8a0909500a1bea1c2b7924758d575b76f
sbx_user1051 <sbx_user1051@169.254.55.165>
2024-07-03 20:20:15 +0000

    Wed Jul 03 20:20:15 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -5,5 +5,4 @@
 def hello(friend_name):
     if not friend_name:
         raise ValueError('no input provided')
-    return f'Hello, {friend_name.capitalize()}!'
-
+    return f'Hello, {friend_name}!'
