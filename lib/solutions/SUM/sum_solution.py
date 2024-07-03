0ccf1a8d2bfe141795738e407531465a40b6170f
sbx_user1051 <sbx_user1051@169.254.119.197>
2024-07-03 16:05:08 +0000

    Wed Jul 03 16:05:08 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,9 +1,10 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if x < 0 or x > 100 or y < 0 or y > 100:
+    if validate_input(x) or validate_input(y):
         raise ValueError('value out of range')
     return x + y
 
 def validate_input(num):
-    
+    return num < 0 or num > 100
+
 
