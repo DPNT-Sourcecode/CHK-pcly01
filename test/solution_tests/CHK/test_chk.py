ae055bc46853a2d4e5b1c96a108f8d533068ac57
sbx_user1051 <sbx_user1051@169.254.55.165>
2024-07-03 20:33:15 +0000

    Wed Jul 03 20:33:15 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -8,9 +8,25 @@ class TestChk():
     def test_chk_three_a_offer(self):
         assert checkout_solution.checkout('A A A') == 130
 
+    def test_chk_double_three_a_offer(self):
+        assert checkout_solution.checkout('A A A A A A') == 260
+
     def test_chk_two_b_offer(self):
         assert checkout_solution.checkout('B B') == 45
 
+    def test_chk_double_two_b_offer(self):
+        assert checkout_solution.checkout('B B B B') == 90
+
+    def test_chk_multiple_different_offers(self):
+        assert checkout_solution.checkout('A A A B B') == 175
+
+    def test_chk_complicated_list(self):
+        assert checkout_solution.checkout('A A A B B C C C D D D') == 280
+
     def test_chk_invalid_letter(self):
+            with pytest.raises(ValueError):
+                checkout_solution.checkout('E')
+
+    def test_chk_no_input(self):
             with pytest.raises(ValueError):
                 checkout_solution.checkout('E')
