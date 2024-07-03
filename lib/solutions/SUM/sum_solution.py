a18352ece6d5b18e4d40f594f78ea80d8e35943d
sbx_user1051 <sbx_user1051@169.254.119.197>
2024-07-03 16:02:08 +0000

    Wed Jul 03 16:02:08 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,9 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if x < 0 
+    if x < 0 or x > 100 or y < 0 or y > 100:
+        raise ValueError('value out of range')
     return x + y
+
+def validate_input(num):
+    
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -12,10 +12,19 @@ class TestSum():
     def test_sum_lowerbounds(self):
         assert sum_solution.compute(0, 0) == 0
 
-    def test_sum_above_limit(self):
+    def test_sum_above_limit_x(self):
         with pytest.raises(ValueError):
             sum_solution.compute(101, 100)
 
-    def test_sum_negative_integer(self):
+    def test_sum_negative_integer_x(self):
         with pytest.raises(ValueError):
             sum_solution.compute(-1, 100)
+
+    def test_sum_above_limit_y(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(100, 101)
+
+    def test_sum_negative_integer_y(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(10, -1)
+
