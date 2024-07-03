d115893f320580385fc6c1a06f421f6c460775c5
sbx_user1051 <sbx_user1051@169.254.55.165>
2024-07-03 20:51:15 +0000

    Wed Jul 03 20:51:15 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,9 +3,13 @@ prices = { 'A': 50,'B': 30,'C': 20,'D': 15 }
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
+    if not skus:
+        raise ValueError('no input provided')
+
     res = {}
 
-    for s in skus.strip():
+    for s in skus.replace(' ','').upper():
         res[s] = res.get(s, 0) + 1
 
     total = 0
@@ -14,3 +18,4 @@ def checkout(skus):
         total += (res[r] * prices[r])
 
     return total
+
