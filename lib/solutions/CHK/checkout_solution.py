8c92e9693088caa1a3ca979092533258ccfff926
sbx_user1051 <sbx_user1051@169.254.75.77>
2024-07-07 20:42:14 +0000

    Sun Jul 07 20:42:14 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,7 @@
 prices = { 'A': 50,'B': 30,'C': 20,'D': 15,'E': 40 }
 offers = { 'A': {'count': 3, 'price': 130 },'B': {'count': 2, 'price': 45 }}
 
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
